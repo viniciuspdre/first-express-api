552c3f243909c4e1ca1a8e71fc58e9db915c4df8
Pedro Lira <pedrovinilira2005@gmail.com>
2025-03-19 23:33:14 -0300

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express from 'express'
 import router from './routes/routes'
-
+import cors from 'cors'
 
 
 export function createApp() {
   const app = express()
 
   app.use(express.json())
-
   app.use("/", router)
+
+
+  const corsOptions = {
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE"]
+  }
+  app.use(cors(corsOptions))
 
   return app
 }
